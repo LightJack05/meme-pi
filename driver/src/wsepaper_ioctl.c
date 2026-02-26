e727d5575c844e3db08934fb68c10306691e48e4
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-02-26 21:38:16 +0100

    Added error handling and fixed CR on kernel printk

--- driver/src/wsepaper_ioctl.c
+++ driver/src/wsepaper_ioctl.c
@@ -6,10 +6,10 @@ long ws_epaper_ioctl(struct file *file, unsigned int cmd,
           unsigned long arg) {
     switch (cmd) {
     case EP_TEST:
-        printk(KERN_INFO "IOCTL received!");
+        printk(KERN_INFO "ws_epaper: IOCTL received!\n");
         break;
     default:
-        printk(KERN_ERR "Unknown IOCTL command: %u", cmd);
+        printk(KERN_ERR "ws_epaper: Unknown IOCTL command: %u\n", cmd);
         return -EINVAL;
     }
     return 0;
--- user/internal/ioctl/test.go
+++ user/internal/ioctl/test.go
@@ -13,9 +13,15 @@ func Test() error {
 		return fmt.Errorf("Could not open /dev/ws_epaper0 for RW: %v", err)
 	}
 
-	_, _, err = unix.Syscall(unix.SYS_IOCTL, devFile.Fd(), 0x2101, 0)
+	_, _, errno := unix.Syscall(unix.SYS_IOCTL, devFile.Fd(), 0x2101, 0)
+	if errno != 0 {
+		return fmt.Errorf("ioctl failed: %v", err)
+	}
 
-	devFile.Close()
+	err = devFile.Close()
+	if err != nil {
+		panic(fmt.Sprintf("Could not close /dev/ws_epaper0: %v", err))
+	}
 
 	return nil
 }
